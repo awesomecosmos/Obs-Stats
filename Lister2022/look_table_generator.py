89f11451dc7365ce4bacbfd131b2eec2c5a7f29c
awesomecosmos <56360465+awesomecosmos@users.noreply.github.com>
2022-01-17 17:26:27 +1300

    attempt at querying elements from JPL :(

--- Lister2022/look_table_generator.py
+++ Lister2022/look_table_generator.py
@@ -1,50 +1,68 @@
 #importing useful packages
 import matplotlib.pyplot as plt
 import numpy as np
-import os
-#---
 from astropy.io import fits
 from astropy.io import ascii
 import astropy.time
 from astropy.time import Time
 import astropy.table
 from astropy.table import Table, Column, MaskedColumn
-#---
 from astroplan import Observer
 from astroquery.jplhorizons import Horizons
-#---
 import pandas as pd
 import csv
+import astropy.time
+import dateutil.parser
 
+### Reading CSV file of dates targets were observed
 raw_target_info = pd.read_csv("Lister2022/look_table.csv")
 
-
-
+### Creating new dataframe with first date each target was observed
 subset_df = raw_target_info[["Target","Date"]]
 grouped_and_first_df = subset_df.groupby(["Target"]).first()
-print(len(grouped_and_first_df))
-print(type(grouped_and_first_df))
+new_df = grouped_and_first_df.reset_index()
+new_df.columns = ["Name","N"]
 
-#print(len(target_val_counts))
-#print(raw_target_info.Target.unique())
-#print(len(raw_target_info.Target.unique()))
+### 
+target_location = '474'
 
-#new_tbl = pd.Series([target_val_counts.iloc[0],target_val_counts.iloc[1]],index=["Name","N"])
-#print(new_tbl.head())
-#target_val_counts = raw_target_info.Target.value_counts().rename_axis("Name").to_frame("N")
-target_val_counts = pd.DataFrame(raw_target_info.Target.value_counts())
-df_val_counts = target_val_counts.reset_index()
-df_val_counts.columns = ["Name","N"]
-#print(df_val_counts)
+obsDate_lst = []
+for i in new_df.N:
+    obsDate_lst.append(i)
 
+targetname_lst = []
+for i in new_df.Name:
+    targetname_lst.append(i)
 
+# need to convert epoch date to julian date
+jd_obsDate_lst = []
+for i in obsDate_lst:
+    dt = dateutil.parser.parse(obsDate_lst[0]) #'2012.11.07'
+    time = astropy.time.Time(dt)
+    jd_obsDate_lst.append(time.jd)
 
-
-#print(subset_df.sort_values(by="Date",ascending=True))
-
-# grouped_df = subset_df.groupby(["Date"]).first()
-# print(grouped_df)
-# raw_target_info.groupby(["Target","Date"]).first()
-#grouped_df = grouped_df.first().sort_values(by="Date",ascending=True)
-#new_grouped_df = 
-#print(grouped_df)
+# #need to query information from JPL Horizons database
+eph_lst = []
+el_lst = []
+obj_lst = []
+for index,jdDate in enumerate(jd_obsDate_lst):
+    if targetname_lst[index] == "C/2018 F4":
+        obj = Horizons(id='90004395', location=target_location,epochs=jdDate)
+    else:
+        obj = Horizons(id=targetname_lst[index], location=target_location,epochs=jdDate)
+    obj_lst.append(obj)
+    # eph_lst.append(obj.ephemerides())
+    # el_lst.append(obj.elements())
+yolo = obj_lst[1]
+print(yolo)
+print(yolo.elements())
+# for i in obj_lst:
+#     try:
+#         print(i.elements())
+#     except:
+#         pass
+# print(el_lst)
+# #now we need to make the JPL data and the FITS data into tables
+# JPLtableData = eph_lst['datetime_str','datetime_jd','RA','DEC','RA_rate','DEC_rate','Tmag','airmass','r','r_rate','delta','delta_rate','elong','alpha','constellation','EL']
+# JPL_colnames = ['datetime_str','datetime_jd','RA','DEC','RA_rate','DEC_rate','Tmag','airmass','r','r_rate','delta','delta_rate','elong','alpha','constellation','EL']
+# JPL_astropy_tbl = Table(JPLtableData,names=JPL_colnames)
